4eaf1e35f3e8af5107045ce87656f379b2296846
root <root@ip-172-31-40-198.eu-central-1.compute.internal>
2017-11-13 09:53:14 +0000

    php

---
+++ profile.php
@@ -0,0 +1,20 @@
+
+<?php
+include('session.php');
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Management Home</title>
+<link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<div id="profile">
+<b id="welcome">Welcome : <i><?php echo $login_session; ?></i></b>
+<b id="logout"><a href="logout.php">Log Out</a></b>
+</div>
+
+
+AWS DynamoDB Update Scripts will be here...
+</body>
+</html>

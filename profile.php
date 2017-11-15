2fc141a00f68646a3637cafd4076e36ce67667bb
oceanosis <dumluufuk@gmail.com>
2017-11-15 15:20:15 +0300

    mysql

--- profile.php
+++ profile.php
@@ -1,4 +1,3 @@
-
 <?php
 include('session.php');
 ?>
@@ -17,9 +16,35 @@ include('session.php');
 AWS DynamoDB Management Table Parameters will be displayed here...
 <br>
 AWS DynamoDB Update Scripts will be here...
+<br>
+All parameters (manTable.json) will be loaded when management Server is created.
+<br>
+<br>
+This form will display retrieved data form dynamodb and let them be updated with a new written update.php action.
+<br>
+<form action="" method="post">
+<label>Update Admin Email :</label>
+<input id="email" name="email" placeholder="adminEmail" type="text">
+<label>Update MyBB Password :</label>
+<input id="password" name="password" placeholder="**********" type="password">
+<input name="submit" type="submit" value=" Update ">
+<span><?php echo $error; ?></span>
+<form>
 
 <br>
 
 Zabbix dashboard stats will be displayed in a frame...
+<br><br>
+<? 
+// Zabbix dashboard url will be the source
+?>
+<frameset cols=”125,*” frameborder=”0”>
+  <frame name=”main” src=”test.php” scrolling=”auto”>
+ <noframes>
+   <body>
+    <h1>Oppps...Web istemcinizin frame desteği yok</h1>
+   </body>
+ </noframes>
+</frameset>
 </body>
 </html>
